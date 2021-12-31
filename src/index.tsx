18bc38a97615cee11d91236b22081e7ecf83b5d6
Eduardo Gustavo <66931597+dudugudu@users.noreply.github.com>
2021-12-31 14:49:22 -0300

    fix 311220211419

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { RootPhone } from './root/phone';
+import { Provider } from "react-redux";
+import store from './store/Main'
+import { BrowserRouter } from "react-router-dom";
+import './GlobalStyle.css'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <Provider store={store} >
+          <RootPhone/>
+      </Provider>
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
--- src/store/ducks/style/actions.ts
+++ src/store/ducks/style/actions.ts
@@ -2,4 +2,4 @@ import { action } from 'typesafe-actions'
 
 import { styleTypes, bg_Main } from './types'
 
-export const updateStyle = (data:bg_Main[])=> action( styleTypes.UPDATED_BACKGROUND,data);
+export const updateStyle = (data:bg_Main)=> action( styleTypes.UPDATED_BACKGROUND,data);
