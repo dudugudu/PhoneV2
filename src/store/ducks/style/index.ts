6236e8cf92956c5d670d4672535607153efdde14
Eduardo Gustavo <66931597+dudugudu@users.noreply.github.com>
2022-01-24 12:37:12 -0300

    atualização de modelos de objetos de style de app

--- src/store/ducks/style/index.ts
+++ src/store/ducks/style/index.ts
@@ -1,46 +1,64 @@
-import { AnyAction, Reducer } from 'redux'
-import { styleState} from './types'
-const INITIAL_STATE: styleState ={
-    navigation:'/',
-    click_position:{
-      x: 0,
-      y: 0,
+import { AnyAction, Reducer } from "redux";
+import { styleState } from "./types";
+const INITIAL_STATE: styleState = {
+  navigation: "/",
+  click_position: {
+    x: 0,
+    y: 0,
+  },
+  phone_main: {
+    what_app: "phone",
+    topBar: { BG_color: "", color: "" },
+    viwer: { BG_color: "", color: "" },
+    baseboard: { BG_color: "", color: "" },
+  },
+  open_app: false,
+  style_apps: [
+    {
+      appName: "keypad",
+      style: [
+        {
+          typestyle: "light",
+          topBar: { BG_color: "#ffffff", color: "#363636" },
+          viwer: { BG_color: "#ffffff", color: "#363636" },
+          baseboard: { BG_color: "#ffffff", color: "#363636" },
+        },
+      ],
     },
-    phone_main:{
-      what_app:'phone',
-      color:'#ffffff',
-      bg_app:'transparent'
-    },
-    open_app: false,
-    style_apps:[
-      { appName:'keypad',
-        style:[
-          {typestyle:'light',bg_color:'#ffffff',color:'#363636'}
-        ]
-      },
-    
-    ]
-   
-}
+  ],
+};
 
-
-
-const Style: Reducer<styleState> = ( state = INITIAL_STATE , action:AnyAction)=>{
+const Style: Reducer<styleState> = (
+  state = INITIAL_STATE,
+  action: AnyAction
+) => {
   switch (action.type) {
-    case 'UPDATED_CLICK_POSITION':
-      return { ...state , click_position:{x: action.x, y: action.y}};
-    case 'OPEN_APP':
-      return { ...state , open_app:action.state};
-    case 'UPDATED_NAVIGATION':
-      return { ...state , navigation:action.navigation}
-    case 'UPDATED_PHONE_MAIN':
-      return { ...state , phone_main:{what_app:action.what_app,color:action.color,bg_app:action.bg_app}}
+    case "UPDATED_CLICK_POSITION":
+      return { ...state, click_position: { x: action.x, y: action.y } };
+    case "OPEN_APP":
+      return { ...state, open_app: action.state };
+    case "UPDATED_NAVIGATION":
+      return { ...state, navigation: action.navigation };
+    case "UPDATED_PHONE_MAIN":
+      return {
+        ...state,
+        phone_main: {
+          what_app: action.what_app,
+          topBar: {
+            BG_color: action.topBar.BG_color,
+            color: action.topBar.color,
+          },
+          viwer: { BG_color: action.viwer.BG_color, color: action.viwer.color },
+          baseboard: {
+            BG_color: action.baseboard.BG_color,
+            color: action.baseboard.color,
+          },
+        },
+      };
 
     default:
       return state;
   }
+};
 
-}
-
-
-export default Style
+export default Style;
--- src/store/ducks/style/types.ts
+++ src/store/ducks/style/types.ts
@@ -1,37 +1,41 @@
 /**
  * Data types
  */
-export interface phone_main{
-    what_app:string;
-    color:string
-    bg_app:string
+export interface phone_main {
+  what_app: string;
+  topBar: templateViwerTypes;
+  viwer: templateViwerTypes;
+  baseboard: templateViwerTypes;
 }
-export interface click_position{
-    x:number,
-    y:number,
-}
-
-interface style{
-    typestyle: string,
-    bg_color:string,
-    color:string,
+export interface click_position {
+  x: number;
+  y: number;
 }
 
-
-export interface style_apps{
-    appName:string,
-    style: style[]
+export interface templateViwerTypes {
+  BG_color: string;
+  color: string;
 }
 
+interface style {
+  typestyle: string;
+  topBar: templateViwerTypes;
+  viwer: templateViwerTypes;
+  baseboard: templateViwerTypes;
+}
 
+export interface style_apps {
+  appName: string;
+  style: style[];
+}
 
 /**
  * State types
  */
-export interface styleState{
-    navigation:string,
-    click_position: click_position,
-    phone_main:phone_main,
-    open_app:boolean
-    style_apps: style_apps[]
+export interface styleState {
+  navigation: string;
+  click_position: click_position;
+  phone_main: phone_main;
+  open_app: boolean;
+  style_apps: style_apps[];
 }
