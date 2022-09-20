e66b3b624c92d2a0eac2ae7b0001246f52c75bcf
Eduardo Gustavo <66931597+dudugudu@users.noreply.github.com>
2022-09-19 23:25:06 -0300

    Update providers.ts

--- src/api/axios/providers.ts
+++ src/api/axios/providers.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const ApiAxios = axios.create({
-  baseURL: "http://177.85.114.132:9090/",
+  baseURL: "http://localhost:9090/",
   timeout: 1000,
   headers: { "Content-Type": "application/json" },
 });
